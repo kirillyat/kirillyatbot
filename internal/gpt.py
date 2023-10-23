09ed5761c22e6de87b2555f71e8d5c2185348744
ksyatsenko <kirillyat@gmail.com>
2023-10-23 23:06:35 +0300

    +

--- internal/gpt.py
+++ internal/gpt.py
@@ -8,17 +8,10 @@ g4f.check_version = False  # Disable automatic version checking
 
 
 
-def gpt3(input: str)->str:
+def gpt4(input: str)->str:
     return _gpt(input, g4f.models.gpt_4)
 
-def gpt4(input: str)->str:
-    return _gpt(input, g4f.models.)
 
-    return g4f.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            provider=g4f.Provider.Aichat,
-            messages=[{"role": "user", "content": input}],
-        )
 
 def _gpt(input: str, mdl)->str:
     if CLOUD == "PYTHONANYWHERE"
